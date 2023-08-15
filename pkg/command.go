e07881287c8df8fb35492426fe34082ced93a70b
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-08-15 16:26:46 -0300

    refactor: ...

--- pkg/command.go
+++ pkg/command.go
@@ -2,39 +2,52 @@ package pkg
 
 import (
 	"flag"
+	"fmt"
 	"strings"
 )
 
 type Command interface {
 	Execute(args []string) (any, error)
+	ParseArgs(args []string) (any, error)
 }
 
 type addCommand struct {
 	service Service
 }
 
-func (c *addCommand) parseArgs(args []string) (string, string, string, string, []string, error) {
+func (c *addCommand) ParseArgs(args []string) (any, error) {
 	flagset := flag.NewFlagSet("add", flag.ExitOnError)
 
 	lang := flagset.String("lang", "", "foreign language")
 	word := flagset.String("word", "", "foreign word")
 	meaning := flagset.String("meaning", "", "translation")
-	example := flagset.String("example", "", "usage example sentence")
 	tags := flagset.String("tags", "", "comma-separated list of tags")
+	example := flagset.String("example", "", "example sentence")
 
 	if err := flagset.Parse(args); err != nil {
-		return "", "", "", "", []string{}, err
+		return nil, err
 	}
 
-	return *lang, *word, *meaning, *example, strings.Split(*tags, ","), nil
+	if *lang == "" {
+		return nil, fmt.Errorf("missing lang")
+	}
+	if *word == "" {
+		return nil, fmt.Errorf("missing word")
+	}
+	if *meaning == "" {
+		return nil, fmt.Errorf("missing meaning")
+	}
+
+	return Word{*lang, *word, *meaning, *example, strings.Split(*tags, ",")}, nil
 }
 
 func (c *addCommand) Execute(args []string) (any, error) {
-	lang, word, meaning, example, tags, err := c.parseArgs(args)
+	values, err := c.ParseArgs(args)
 	if err != nil {
 		return nil, err
 	}
-	return c.service.AddWord(lang, word, meaning, example, tags)
+	word := values.(Word)
+	return c.service.AddWord(word.Lang, word.Word, word.Meaning, word.Example, word.Tags)
 }
 
 func CreateAddCommand(service Service) Command {
--- pkg/command_test.go
+++ pkg/command_test.go
@@ -8,13 +8,34 @@ import (
 )
 
 func TestAddCommand(t *testing.T) {
-	expected := &pkg.Word{"german", "Hallo", "Hello", "Hallo, wie gehts", []string{"greetings"}}
+	t.Run("add", func(t *testing.T) {
+		expected := &pkg.Word{"german", "Hallo", "Hello", "Hallo, wie gehts", []string{"greetings"}}
 
-	cmd := pkg.CreateAddCommand(pkg.NewService(pkg.NewInMemoryRepository()))
+		cmd := pkg.CreateAddCommand(pkg.NewService(pkg.NewInMemoryRepository()))
 
-	received, _ := cmd.Execute([]string{"-lang", "german", "-word", "Hallo", "-meaning", "Hello", "-example", "Hallo, wie gehts", "-tags", "greetings"})
+		received, _ := cmd.Execute([]string{"-lang", "german", "-word", "Hallo", "-meaning", "Hello", "-example", "Hallo, wie gehts", "-tags", "greetings"})
 
-	if !reflect.DeepEqual(expected, received) {
-		t.Errorf("expected %v, got %v", expected, received)
-	}
+		if !reflect.DeepEqual(expected, received) {
+			t.Errorf("expected %v, got %v", expected, received)
+		}
+	})
+
+	t.Run("required", func(t *testing.T) {
+		cmd := pkg.CreateAddCommand(pkg.NewService(pkg.NewInMemoryRepository()))
+
+		_, err := cmd.Execute([]string{"-word", "Hallo", "-meaning", "Hello", "-example", "Hallo, wie gehts", "-tags", "greetings"})
+		if err.Error() != "missing lang" {
+			t.Errorf("expected error: missing lang, got: %s", err)
+		}
+
+		_, err = cmd.Execute([]string{"-lang", "german", "-meaning", "Hello", "-example", "Hallo, wie gehts", "-tags", "greetings"})
+		if err.Error() != "missing word" {
+			t.Errorf("expected error: missing word, got: %s", err)
+		}
+
+		_, err = cmd.Execute([]string{"-lang", "german", "-word", "Hallo", "-example", "Hallo, wie gehts", "-tags", "greetings"})
+		if err.Error() != "missing meaning" {
+			t.Errorf("expected error: missing meaning, got: %s", err)
+		}
+	})
 }
