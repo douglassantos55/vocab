7b92d1674eafe4de22759dc0b1661108fe97f86b
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-08-23 21:45:28 -0300

    feat: put things together

--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"example.com/gocab/pkg"
+)
+
 func main() {
-	print("...\n")
+	if len(os.Args) < 2 {
+		print("expected 'add', 'update' or 'quiz' subcommands")
+		os.Exit(1)
+	}
+
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	databaseDir := path.Join(configDir, "gocab")
+	_, err = os.ReadDir(databaseDir)
+	if err != nil {
+		if err := os.Mkdir(databaseDir, 0755); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	filename := path.Join(databaseDir, "database.db")
+	repository, err := pkg.NewSqliteRepository(fmt.Sprintf("file:%s?cache=shared", filename))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer repository.Close()
+	service := pkg.NewService(repository)
+
+	commands := make(map[string]pkg.Command)
+	commands["add"] = pkg.CreateAddCommand(service, pkg.StdWordArgsParser)
+	commands["update"] = pkg.CreateUpdateCommand(service, pkg.StdWordArgsParser)
+	commands["quiz"] = pkg.CreateQuizCommand(service, pkg.StdQuizArgsParser, os.Stdin, os.Stdout)
+
+	command, ok := commands[os.Args[1]]
+	if !ok {
+		fmt.Println("INVALID COMAND")
+		return
+	}
+
+	if _, err := command.Execute(os.Args[2:]); err != nil {
+		fmt.Println(err)
+	}
 }
